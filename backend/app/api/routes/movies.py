535446bec1d3fa61f17ec73bf818158613031795
victormoraes <victor.moraesrj@gmail.com>
2024-08-30 17:04:41 -0300

    refactor: commented endpoints

--- backend/app/api/routes/movies.py
+++ backend/app/api/routes/movies.py
@@ -9,14 +9,20 @@ def get_tmdb_service():
 
 @router.get("/search")
 async def search_movies(movie_title: str = Query(..., description="Title of the movie to search for"), tmdb_service: TMDBService = Depends(get_tmdb_service)):
+    """
+    Search for movies on TMDB.
+    """
     movie_data = await tmdb_service.get_movies(movie_title)
     if "Error" in movie_data:
         raise HTTPException(status_code=404, detail=movie_data["Error"])
     return movie_data
 
 @router.get("/{movie_id}")
-async def get_movie(movie_id: int, tmdb_service: TMDBService = Depends(get_tmdb_service)):
-    movie_data = await tmdb_service.get_movie_by_id(movie_id)
+async def get_movie_by_id(movie_id: int, tmdb_service: TMDBService = Depends(get_tmdb_service)):
+    """
+    Get a movie by ID from TMDB.
+    """
+    movie_data = await tmdb_service.get_movie(movie_id)
     if "Error" in movie_data:
         raise HTTPException(status_code=404, detail=movie_data["Error"])
     return movie_data

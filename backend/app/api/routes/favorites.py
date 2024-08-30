ca6c3ac732555a1bdddf0596309edf1d64a35067
victormoraes <victor.moraesrj@gmail.com>
2024-08-30 16:52:39 -0300

    feat: TMDB API response retuning proper HTTP exceptions

--- backend/app/api/routes/favorites.py
+++ backend/app/api/routes/favorites.py
@@ -1,4 +1,5 @@
 # app/api/routes.py
+from app.services.tmdb_service import TMDBService
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
@@ -7,6 +8,10 @@ from app.core.db import get_db
 
 
 router = APIRouter()
+
+def get_tmdb_service():
+    return TMDBService()
+
 
 @router.post("/", response_model=schemas.FavoriteListResponse)
 def create_favorite_list(favorite_list: schemas.FavoriteListCreate, db: Session = Depends(get_db)):
@@ -42,18 +47,6 @@ def delete_favorite_list(favorite_list_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Favorite list not found")
     return {"message": "Favorite list deleted successfully"}
 
-@router.post("/{favorite_list_id}/add_movie/", response_model=schemas.FavoriteListResponse)
-def add_movie_to_favorite_list(favorite_list_id: int, movie_id: int, db: Session = Depends(get_db)):
-    db_favorite_list, error = crud.add_movie_to_favorite_list(db, favorite_list_id, movie_id)
-    
-    if error == "Movie not found":
-        raise HTTPException(status_code=404, detail="Movie not found")
-    elif error == "Favorite list not found":
-        raise HTTPException(status_code=404, detail="Favorite list not found")
-    elif error == "Movie already in favorite list":
-        raise HTTPException(status_code=400, detail="Movie already in favorite list")
-    
-    return db_favorite_list
 
 
 @router.delete("/{favorite_list_id}/remove_movie/", response_model=schemas.FavoriteListResponse)
@@ -64,5 +57,37 @@ def remove_movie_from_favorite_list(favorite_list_id: int, movie_id: int, db: Se
         raise HTTPException(status_code=404, detail="Favorite list not found")
     elif error == "Movie not in favorite list":
         raise HTTPException(status_code=400, detail="Movie not in favorite list")
+    
+    return db_favorite_list
+
+
+@router.post("/{favorite_list_id}/add_movie/", response_model=schemas.FavoriteListResponse)
+async def add_movie_to_favorite_list(
+    favorite_list_id: int, 
+    external_id: int, 
+    db: Session = Depends(get_db),
+    tmdb_service: TMDBService = Depends(get_tmdb_service)
+):
+    movie = db.query(models.Movie).filter(models.Movie.external_id == str(external_id)).first()
+    # Se o filme não estiver no banco de dados, busca na API TMDB e o cria
+    if not movie:
+        movie_data = await tmdb_service.get_movie_by_id(external_id)
+        if "Error" in movie_data:
+            raise HTTPException(status_code=404, detail="Movie not found in TMDB")
+        movie = crud.create_movie_from_api_data(db, movie_data)
+    
+    # Verifica se a lista de favoritos existe
+    db_favorite_list = db.query(models.FavoriteList).filter(models.FavoriteList.id == favorite_list_id).first()
+    if not db_favorite_list:
+        raise HTTPException(status_code=404, detail="Favorite list not found")
+    
+    # Verifica se o filme já está na lista de favoritos
+    if movie in db_favorite_list.movies:
+        raise HTTPException(status_code=400, detail="Movie already in favorite list")
+    
+    # Adiciona o filme à lista de favoritos
+    db_favorite_list.movies.append(movie)
+    db.commit()
+    db.refresh(db_favorite_list)
     
     return db_favorite_list
--- backend/app/services/tmdb_service.py
+++ backend/app/services/tmdb_service.py
@@ -1,5 +1,6 @@
 import httpx
 from ..core.config import settings
+from fastapi import HTTPException
 
 
 class TMDBService:
@@ -11,32 +12,40 @@ class TMDBService:
         }
 
     async def get_movies(self, movie_title: str):
-            url = f"{self.api_base_url}/search/movie"
-            params = {
-                "query": movie_title,
-                "language": "en-US",
-                "include_adult": "false",
-                "page": 1
-            }
-            
+        url = f"{self.api_base_url}/search/movie"
+        params = {
+            "query": movie_title,
+            "language": "en-US",
+            "include_adult": "false",
+            "page": 1
+        }
+        
+        try:
             async with httpx.AsyncClient() as client:
                 response = await client.get(url, headers=self.headers, params=params)
-                response.raise_for_status()  # Levanta uma exceção se a resposta não for 200 (OK)
+                response.raise_for_status()  # Levanta uma exceção se a resposta for um código de erro HTTP
                 data = response.json()
-            
+                
             if data["results"]:
                 return data["results"]
             else:
-                return {"Error": "Movie not found"}
+                raise HTTPException(status_code=404, detail="Movie not found")
+        
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                raise HTTPException(status_code=404, detail="Movie not found in TMDB")
+            raise HTTPException(status_code=500, detail="An error occurred while searching for the movie on TMDB")
             
     async def get_movie_by_id(self, movie_id: int):
         url = f"{self.api_base_url}/movie/{movie_id}"
         
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, headers=self.headers)
-            response.raise_for_status()
-            data = response.json()
-        if not data:
-            return {"Error": "Movie not found"}
-        
-        return data
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(url, headers=self.headers)
+                response.raise_for_status()  # Levanta um erro se a resposta for um código de erro HTTP
+                return response.json()
+            
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                raise HTTPException(status_code=404, detail="Movie not found in TMDB")
+            raise HTTPException(status_code=500, detail="An error occurred while fetching the movie from TMDB")

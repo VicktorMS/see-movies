9d583194afaef904eaafd5ac5339da4df8fc0cfb
victormoraes <victor.moraesrj@gmail.com>
2024-08-31 18:18:24 -0300

    feat: created main layout components

---
+++ frontend/src/app/ui/layouts/header.js
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (
+        <header className="navbar bg-primary text-primary-content shadow-md p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="text-2xl font-bold">See Movies</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header

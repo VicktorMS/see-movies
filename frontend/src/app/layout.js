f5317ca058101db650e597ca810d2e1088dbb96c
victormoraes <victor.moraesrj@gmail.com>
2024-09-01 19:05:43 -0300

    refactor: changed the application theme to light

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR" data-theme="bumblebee">
+    <html lang="pt-BR" data-theme="light">
       <body className={`${inter.className}`}>
         <Header />
         <main className="container mx-auto py-6 px-4">
--- frontend/src/app/ui/title.js
+++ frontend/src/app/ui/title.js
@@ -1,7 +1,7 @@
 
 function Title({children}) {
   return (
-    <h2 className="text-3xl font-bold text-primary-content my-4">{children}</h2>
+    <h2 className="text-3xl font-bold my-4">{children}</h2>
   )
 }
 

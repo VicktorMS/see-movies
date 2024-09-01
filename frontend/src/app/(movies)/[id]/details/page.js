f99cbd1eb29528bfa85866ca0696599e7e5ca607
victormoraes <victor.moraesrj@gmail.com>
2024-09-01 11:04:23 -0300

    refactor

--- frontend/src/app/(movies)/[id]/details/page.js
+++ frontend/src/app/(movies)/[id]/details/page.js
@@ -1,9 +1,24 @@
 import Title from "@/app/ui/title";
+import { fetchMovieDetailsById } from "@/app/lib/data";
 
-export default async function Page() {
-  return (
-    <main>
-      <Title>Principais Filmes</Title>
-    </main>
-  );
+export default async function Page({ params }) {
+  const { id } = params;
+
+  try {
+    const movie_details = await fetchMovieDetailsById(id);
+
+    return (
+      <main>
+        <Title>{movie_details?.title}</Title>
+        {/* Render movie details here */}
+      </main>
+    );
+  } catch (error) {
+    return (
+      <main>
+        <Title>Erro ao carregar o filme</Title>
+        <p>{error.message}</p>
+      </main>
+    );
+  }
 }

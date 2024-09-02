a8c474d91a9fff0b5985df2f5725ef59cfdcb4a6
victormoraes <victor.moraesrj@gmail.com>
2024-09-02 11:41:20 -0300

    refactor: Refactor CreateFavoriteListDrawer component Modularize form handling and validation

--- frontend/src/app/ui/favorites/create-favoritelist-drawer.js
+++ frontend/src/app/ui/favorites/create-favoritelist-drawer.js
@@ -1,21 +1,21 @@
+'use client';
 import React, { useState } from 'react';
 import * as Yup from 'yup';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import { createFavoriteList } from '@/app/lib/data';
 import Title from '@/app/ui/title';
 import BottomDrawer from "@/app/ui/bottom-drawer";
 
-
+// Validation schema using Yup
 const validationSchema = Yup.object().shape({
   title: Yup.string().required('Título é obrigatório').min(3, 'O título deve ter pelo menos 3 caracteres'),
   description: Yup.string().required('Descrição é obrigatória').min(5, 'A descrição deve ter pelo menos 5 caracteres'),
 });
 
-function CreateFavoriteListDrawer({ isOpen, onClose }) {
+export default function CreateFavoriteListDrawer({ isOpen, onClose }) {
   const [formData, setFormData] = useState({ title: '', description: '' });
   const [errors, setErrors] = useState({});
-
-  const router = useRouter()
+  const router = useRouter();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,19 +27,17 @@ function CreateFavoriteListDrawer({ isOpen, onClose }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const isValid = await validateFormData();
+    if (isValid) {
+      await submitFormData();
+    }
+  };
 
+  const validateFormData = async () => {
     try {
       await validationSchema.validate(formData, { abortEarly: false });
-
-      const result = await createFavoriteList(formData.title, formData.description);
-      
-      if (result) {
-        setFormData({ title: '', description: '' });
-        onClose();
-        router.push('/favorites/' + result.id + '/details');
-      } else {
-        console.log('Erro ao criar a lista de favoritos');
-      }
+      setErrors({});
+      return true;
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         const validationErrors = {};
@@ -48,52 +46,50 @@ function CreateFavoriteListDrawer({ isOpen, onClose }) {
         });
         setErrors(validationErrors);
       } else {
-        console.error('Erro inesperado:', error);
+        console.error('Unexpected error:', error);
       }
+      return false;
     }
   };
 
+  const submitFormData = async () => {
+    const result = await createFavoriteList(formData.title, formData.description);
+    if (result) {
+      resetForm();
+      onClose();
+      router.push(`/favorites/${result.id}/details`);
+    } else {
+      console.log('Error creating the favorite list');
+    }
+  };
+
+  const resetForm = () => {
+    setFormData({ title: '', description: '' });
+    setErrors({});
+  };
+
   return (
-    <BottomDrawer isOpen={isOpen} onClose={onClose} >
+    <BottomDrawer isOpen={isOpen} onClose={onClose}>
       <Title>Criar Nova Lista de Favoritos</Title>
       <div className="divider"></div>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-center h-full'>
-        <label className="form-control w-full max-w-md">
-          <div className='label'>
-            <span className='label-text text-lg'>
-              Título da Lista de Favoritos
-            </span>
-          </div>
-          <input
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            className="input input-bordered w-full max-w-md"
-            placeholder="Digite o título"
-          />
-          {errors.title && <span className="text-red-500 text-sm">{errors.title}</span>}
-        </label>
-        <label className="form-control w-full max-w-md">
-          <div className='label'>
-            <span className='label-text text-lg'>
-              Descrição
-            </span>
-          </div>
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            className="input input-bordered w-full max-w-md"
-            placeholder="Digite a descrição"
-          />
-          {errors.description && <span className="text-red-500 text-sm">{errors.description}</span>}
-        </label>
-        <button
-          type="submit"
-          className="btn btn-primary mt-4"
-        >
+      <form onSubmit={handleSubmit} className="flex flex-col justify-center h-full">
+        <FormInput
+          label="Título da Lista de Favoritos"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+          error={errors.title}
+          placeholder="Digite o título"
+        />
+        <FormInput
+          label="Descrição"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          error={errors.description}
+          placeholder="Digite a descrição"
+        />
+        <button type="submit" className="btn btn-primary mt-4">
           Criar Lista
         </button>
       </form>
@@ -101,4 +97,22 @@ function CreateFavoriteListDrawer({ isOpen, onClose }) {
   );
 }
 
-export default CreateFavoriteListDrawer;
+// Component for rendering input fields with error messages
+function FormInput({ label, name, value, onChange, error, placeholder }) {
+  return (
+    <label className="form-control w-full max-w-md">
+      <div className="label">
+        <span className="label-text text-lg">{label}</span>
+      </div>
+      <input
+        type="text"
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="input input-bordered w-full max-w-md"
+        placeholder={placeholder}
+      />
+      {error && <span className="text-red-500 text-sm">{error}</span>}
+    </label>
+  );
+}
